c4269e562bb1305e0dbe58723a6835254196bafb
SwarnaHG <swarnahg18@gmail.com>
2022-07-27 12:03:15 +0530

    Create contacts in Address Book

---
+++ src/AddressBook/AddressBook.java
@@ -0,0 +1,44 @@
+package AddressBook;
+
+public class AddressBook {
+    private class Contact {
+        String firstName, lastNmae, address, city, state, emailId;
+        int zipCode;
+        long mobileNumber;
+    }
+
+    /**
+     * created method printContact for creating contacts in AddressBook
+     */
+    public void printContact() {
+        Contact person = new Contact();
+        person.firstName = "Swarna";
+        person.lastNmae = "HG";
+        person.address = "BTM LAYOUT";
+        person.city = "Bangalore";
+        person.state = "Karnataka";
+        person.zipCode = 577206;
+        person.mobileNumber = 9113919512L;
+        person.emailId = "swarnahg18@gmail.com";
+        System.out.println("Contact Details");
+        System.out.println("Name: " + person.firstName + " " + person.lastNmae + "\n" + "Address: "
+                + person.address + "\n" + "City: " + person.city + "\n" + "State: " + person.state
+                + "\n" + "ZipCode: " + person.zipCode + "\n" + "Mobile Number: " + person.mobileNumber + "\n"
+                + "EmailId: " + person.emailId + "\n");
+    }
+
+
+    public static void main(String[] args) {
+
+         //instance of AddressBook class
+
+        AddressBook addressBook = new AddressBook();
+
+         //DisplayContact
+
+        addressBook.printContact();
+
+    }
+
+
+}
